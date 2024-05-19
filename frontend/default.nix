f006ac6aaafc4e4f326fefaaa99818464c15e821
mystieu <mzhou11@ocdsb.ca>
2024-05-19 07:45:26 -0400

    FIXING EVERYTHING.

--- frontend/default.nix
+++
@@ -1,8 +0,0 @@
-{ nodejs, buildNpmPackage, ... }:
-
-buildNpmPackage {
-   name = "frontend";
-   inherit nodejs;
-   npmDepsHash = "sha256-R6sJIGV9ywdwvYrdLP5gazImVLMGbapm9nELB9UTPQk=";
-   src = ./.;
-}
