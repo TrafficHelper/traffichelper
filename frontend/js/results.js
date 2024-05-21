12ca920aa8fb127613521a8cf9eea1fd4516a96b
mystieu <mzhou11@ocdsb.ca>
2024-05-21 00:52:13 -0400

    light mode maps

--- frontend/js/results.js
+++ frontend/js/results.js
@@ -3,10 +3,9 @@ var results_map = L.map('results-map', {
     zoom: 15
 }).setView([45.424721, -75.695000], 12);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 20
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+    maxZoom: 20,
 }).addTo(results_map);
 
 function getRandomColor() {
