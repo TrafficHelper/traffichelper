24e365170d3fa13af21fe60c1a79447dbfdd8fb1
mystieu <mzhou11@ocdsb.ca>
2024-05-23 23:21:32 -0400

    up

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -3,9 +3,10 @@ var map = L.map('map', {
     zoom: 15
 }).setView([45.424721, -75.695000], 12);
 
-L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
     maxZoom: 20,
 }).addTo(map);
 
-//https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png
+// //dark mode (map)
+//https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png //light mode (map)
